d8b8fc83439f234447d75ab61df0f8d9a19544f9
kfcampbell <kfcampbell@github.com>
2021-10-11 16:27:34 -0700

    Remove faulty implementation so main works

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
-	"time"
 
+	"github.com/kfcampbell/single-digits/parser"
 	"github.com/otiai10/gosseract/v2"
 )
 
@@ -28,37 +27,6 @@ func GetText(imgPath string) (string, error) {
 	return text, nil
 }
 
-// GetScoreFromText extracts the time from the boilerplate NYT text
-// and returns it in seconds.
-func GetScoreFromText(text string) (time.Duration, error) {
-	const dividerText = "You solved a Mini puzzle in "
-	s := strings.Split(text, dividerText)
-	scoreText := s[1]
-	// case when it's formatted like "35 seconds."
-	if strings.Contains(scoreText, "seconds") {
-		timePieces := strings.Split(scoreText, " ")
-		secondsS := timePieces[0]
-		return time.ParseDuration(secondsS + "s")
-	} else { // case when it's formatted like "1:42"
-		timePieces := strings.Split(scoreText, ":")
-		minutesS := timePieces[0]
-		secondsPieces := timePieces[1]
-		secondsS := strings.Split(secondsPieces, ".")
-
-		minutes, err := time.ParseDuration(minutesS + "m")
-		if err != nil {
-			return 0, err
-		}
-
-		seconds, err := time.ParseDuration(secondsS[0] + "s")
-		if err != nil {
-			return 0, err
-		}
-
-		return minutes + seconds, nil
-	}
-}
-
 func run() error {
 	client := gosseract.NewClient()
 	defer client.Close()
@@ -68,7 +36,7 @@ func run() error {
 		return err
 	}
 	fmt.Printf("%s\n", text)
-	score, err := GetScoreFromText(text)
+	score, err := parser.GetScoreFromText(text)
 	if err != nil {
 		return err
 	}
@@ -79,7 +47,7 @@ func run() error {
 		return err
 	}
 	fmt.Printf("%s\n", text)
-	score, err = GetScoreFromText(text)
+	score, err = parser.GetScoreFromText(text)
 	if err != nil {
 		return err
 	}
@@ -90,33 +58,33 @@ func run() error {
 		return err
 	}
 	fmt.Printf("%s\n", text)
-	/*score, err = GetScoreFromText(text)
+	score, err = parser.GetScoreFromText(text)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("score: %v\n", score)*/
+	fmt.Printf("score: %v\n", score)
 
 	text, err = GetText("testdata/thirty-six-seconds.jpeg")
 	if err != nil {
 		return err
 	}
 	fmt.Printf("%s\n", text)
-	/*score, err = GetScoreFromText(text)
+	score, err = parser.GetScoreFromText(text)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("score: %v\n", score)*/
+	fmt.Printf("score: %v\n", score)
 
 	text, err = GetText("testdata/twenty-three-seconds.jpeg")
 	if err != nil {
 		return err
 	}
 	fmt.Printf("%s\n", text)
-	/*score, err = GetScoreFromText(text)
+	score, err = parser.GetScoreFromText(text)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("score: %v\n", score)*/
+	fmt.Printf("score: %v\n", score)
 
 	return nil
 }
