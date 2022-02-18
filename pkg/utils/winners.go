c206def80673ee2dd5b78562a9bc51152e02d0a1
Keegan Campbell <kfcampbell@github.com>
2022-02-18 09:29:32 -0800

    Refactor to enable better testing

---
+++ pkg/utils/winners.go
@@ -0,0 +1,63 @@
+package utils
+
+import (
+	"fmt"
+
+	sc "github.com/kfcampbell/single-digits/pkg/score"
+)
+
+// GetWinnersMessage takes a list of Score structs, sorts them, and returns
+// a nicely formatted podium string.
+// todo: handle case where there's only three submissions and it includes a tie
+func GetWinnersMessage(scores []sc.Score, date string) string {
+	scores = sc.SortScores(scores)
+
+	// tie for first place
+	if scores[0].Score == scores[1].Score {
+		return fmt.Sprintf(`
+		Results for %v:
+		🥇 - tie for first! %v and %v with times of %v
+		🥈 - %v with a time of %v
+		🥉 - %v with a time of %v
+		`, date,
+			scores[0].Author, scores[1].Author, scores[0].Score,
+			scores[2].Author, scores[2].Score,
+			scores[3].Author, scores[3].Score)
+	}
+
+	// tie for second place
+	if scores[1].Score == scores[2].Score {
+		return fmt.Sprintf(`
+		Results for %v:
+		🥇 - %v with a time of %v
+		🥈 - tie for second! %v and %v with times of %v
+		🥉 - %v with a time of %v
+		`, date,
+			scores[0].Author, scores[0].Score,
+			scores[1].Author, scores[2].Author, scores[1].Score,
+			scores[3].Author, scores[3].Score)
+	}
+
+	// tie for third place
+	if len(scores) > 3 && scores[2].Score == scores[3].Score {
+		return fmt.Sprintf(`
+		Results for %v:
+		🥇 - %v with a time of %v
+		🥈 - %v with a time of %v
+		🥉 - tie for third! %v and %v with times of %v
+		`, date,
+			scores[0].Author, scores[0].Score,
+			scores[1].Author, scores[1].Score,
+			scores[2].Author, scores[3].Author, scores[2].Score)
+	}
+	return fmt.Sprintf(`
+		Results for %v:
+	🥇 - %v with a time of %v
+	🥈 - %v with a time of %v
+	🥉 - %v with a time of %v
+	`, date,
+		scores[0].Author, scores[0].Score,
+		scores[1].Author, scores[1].Score,
+		scores[2].Author, scores[2].Score)
+
+}
